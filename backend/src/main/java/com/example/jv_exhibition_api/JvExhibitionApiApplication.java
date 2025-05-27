fed6ddc7946bc4bbe1d962df5198e84e92618aa3
Kye <NovusAa12@proton.me>
2025-05-27 14:02:59 +0100

    chore: initial commit

---
+++ backend/src/main/java/com/example/jv_exhibition_api/JvExhibitionApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.jv_exhibition_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvExhibitionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvExhibitionApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/jv_exhibition_api/JvExhibtionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jv_exhibition_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvExhibtionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
