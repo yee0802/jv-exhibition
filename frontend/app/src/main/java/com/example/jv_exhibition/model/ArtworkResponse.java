f224ec795300977f7d64fa87b9bd7b8a5a6f301a
Kye <NovusAa12@proton.me>
2025-05-29 18:00:24 +0100

    feat: add ArtworkResponse data model

---
+++ frontend/app/src/main/java/com/example/jv_exhibition/model/ArtworkResponse.java
@@ -0,0 +1,60 @@
+package com.example.jv_exhibition.model;
+
+import java.util.List;
+
+public class ArtworkResponse {
+    private List<Artwork> content;
+    private int pageNo;
+    private int pageSize;
+    private int totalElements;
+    private int totalPages;
+    private boolean last;
+
+    public List<Artwork> getContent() {
+        return content;
+    }
+
+    public void setContent(List<Artwork> content) {
+        this.content = content;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public void setPageNo(int pageNo) {
+        this.pageNo = pageNo;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public int getTotalElements() {
+        return totalElements;
+    }
+
+    public void setTotalElements(int totalElements) {
+        this.totalElements = totalElements;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public boolean isLast() {
+        return last;
+    }
+
+    public void setLast(boolean last) {
+        this.last = last;
+    }
+}
