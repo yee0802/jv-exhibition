fa1f55ca450c777d2b450d9cb49048b09f22bbfd
Kye <NovusAa12@proton.me>
2025-05-29 18:05:41 +0100

    feat: add ArtworkApiService

---
+++ frontend/app/src/main/java/com/example/jv_exhibition/service/ArtworkApiService.java
@@ -0,0 +1,12 @@
+package com.example.jv_exhibition.service;
+
+import com.example.jv_exhibition.model.ArtworkResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ArtworkApiService {
+    @GET("artworks")
+    Call<ArtworkResponse> getAllArtworks(@Query("pageNo") int pageNo, @Query("pageSize") int pageSize);
+}
