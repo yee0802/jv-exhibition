c61dcbc11709ce1eb0b4f90867aa9e20e184ab7f
Kye <NovusAa12@proton.me>
2025-05-29 18:09:00 +0100

    feat: add ArtworkRepository

---
+++ frontend/app/src/main/java/com/example/jv_exhibition/repository/ArtworkRepository.java
@@ -0,0 +1,43 @@
+package com.example.jv_exhibition.repository;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.jv_exhibition.model.ArtworkResponse;
+import com.example.jv_exhibition.service.ArtworkApiService;
+import com.example.jv_exhibition.service.RetrofitInstance;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class ArtworkRepository {
+    private final String TAG = "ArtworkRepository";
+    private final ArtworkApiService artworkApiService;
+    private MutableLiveData<ArtworkResponse> artworkResponseLiveData = new MutableLiveData<>();
+
+    public ArtworkRepository(Application application) {
+        this.artworkApiService = RetrofitInstance.getService();
+    }
+
+    public void loadArtworks(int pageNo, int pageSize) {
+        Call<ArtworkResponse> call = artworkApiService.getAllArtworks(pageNo, pageSize);
+        call.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<ArtworkResponse> call, Response<ArtworkResponse> response) {
+                artworkResponseLiveData.postValue(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<ArtworkResponse> call, Throwable t) {
+                Log.e(TAG, "Failed to fetch artworks", t);
+            }
+        });
+    }
+
+    public MutableLiveData<ArtworkResponse> getArtworksLiveData() {
+        return artworkResponseLiveData;
+    }
+}
