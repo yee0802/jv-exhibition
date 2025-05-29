050542c93c7ef32ba77ec3479676878de7f939e4
Kye <NovusAa12@proton.me>
2025-05-29 17:56:01 +0100

    feat: add RetroFitInstance

---
+++ frontend/app/src/main/java/com/example/jv_exhibition/service/RetrofitInstance.java
@@ -0,0 +1,29 @@
+package com.example.jv_exhibition.service;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static RetrofitInstance retrofitInstance = null;
+    private static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
+
+    public static ArtworkApiService getService() {
+        if (retrofitInstance == null) {
+            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            return retrofit.create(ArtworkApiService.class);
+        }
+
+        return null;
+    }
+}
