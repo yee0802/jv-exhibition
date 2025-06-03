93dd5012a5bd260482ffccaa55f5e43514f076a8
Kye <NovusAa12@proton.me>
2025-06-03 15:12:44 +0100

    fix: update id type in ArtworkRepository

--- backend/src/main/java/com/example/jv_exhibition_api/repository/ArtworkRepository.java
+++ backend/src/main/java/com/example/jv_exhibition_api/repository/ArtworkRepository.java
@@ -5,4 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArtworkRepository extends JpaRepository<Artwork, Long> {}
+public interface ArtworkRepository extends JpaRepository<Artwork, String> {}
