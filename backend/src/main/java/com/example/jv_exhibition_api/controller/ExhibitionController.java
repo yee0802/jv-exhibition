0067732a66a5fb66db5654c41fc4c91829a1223e
Kye <NovusAa12@proton.me>
2025-06-03 20:30:47 +0100

    feat: add ExhibitionController

---
+++ backend/src/main/java/com/example/jv_exhibition_api/controller/ExhibitionController.java
@@ -0,0 +1,41 @@
+package com.example.jv_exhibition_api.controller;
+
+import com.example.jv_exhibition_api.dto.ArtworkDTO;
+import com.example.jv_exhibition_api.dto.ExhibitionDTO;
+import com.example.jv_exhibition_api.model.Exhibition;
+import com.example.jv_exhibition_api.service.ExhibitionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/exhibitions")
+public class ExhibitionController {
+
+    @Autowired
+    private ExhibitionService exhibitionService;
+
+    @GetMapping("/{exhibitionId}")
+    public ResponseEntity<Exhibition> getExhibitionById(@PathVariable Long exhibitionId) {
+        return new ResponseEntity<>(exhibitionService.getExhibitionById(exhibitionId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Exhibition> createExhibition(@RequestBody ExhibitionDTO exhibitionDTO) {
+        return new ResponseEntity<>(exhibitionService.createExhibition(exhibitionDTO), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{exhibitionId}/remove")
+    public ResponseEntity<Exhibition> removeArtworkFromExhibition(
+            @PathVariable Long exhibitionId, @RequestBody ArtworkDTO artworkDTO) {
+        return new ResponseEntity<>(exhibitionService.removeArtworkFromExhibition(exhibitionId, artworkDTO), HttpStatus.OK);
+    }
+
+    @PostMapping("/{exhibitionId}/add")
+    public ResponseEntity<Exhibition> addArtworkToExhibition(
+            @PathVariable Long exhibitionId, @RequestBody ArtworkDTO artworkDTO) {
+        return new ResponseEntity<>(exhibitionService.addArtworkToExhibition(exhibitionId, artworkDTO), HttpStatus.OK);
+    }
+}
+
