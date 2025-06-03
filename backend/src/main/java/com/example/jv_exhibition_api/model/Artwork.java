e3f4e722097688ae8f5c6b9c4208cba1c8d6d410
Kye <NovusAa12@proton.me>
2025-06-03 16:08:56 +0100

    refactor: integrate lombok

--- backend/src/main/java/com/example/jv_exhibition_api/model/Artwork.java
+++ backend/src/main/java/com/example/jv_exhibition_api/model/Artwork.java
@@ -1,7 +1,9 @@
 package com.example.jv_exhibition_api.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
 @Table(name = "artworks")
 @Entity
 public class Artwork {
@@ -28,82 +30,4 @@ public class Artwork {
 
     @Column(columnDefinition="text")
     private String medium;
-
-    public Artwork() {
-    }
-
-    public Artwork(String id, String medium, String date, String source, String imageUrl, String artist, String description, String title) {
-        this.id = id;
-        this.medium = medium;
-        this.date = date;
-        this.source = source;
-        this.imageUrl = imageUrl;
-        this.artist = artist;
-        this.description = description;
-        this.title = title;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getMedium() {
-        return medium;
-    }
-
-    public void setMedium(String medium) {
-        this.medium = medium;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getArtist() {
-        return artist;
-    }
-
-    public void setArtist(String artist) {
-        this.artist = artist;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
 }
