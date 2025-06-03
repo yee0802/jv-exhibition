38654b92bc7aad59f22b3dba02fb94a3c7655bd0
Kye <NovusAa12@proton.me>
2025-06-03 20:18:43 +0100

    feat: add GlobalExceptionHandler

---
+++ backend/src/main/java/com/example/jv_exhibition_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.example.jv_exhibition_api.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleItemNotFoundException(ItemNotFoundException e) {
+        return new ResponseEntity<>(new ErrorResponse(404, e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleMissingFieldException(MissingFieldException e) {
+        return new ResponseEntity<>(new ErrorResponse(400, e.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleUnavailableRouteException(UnavailableRouteException e) {
+        return new ResponseEntity<>(new ErrorResponse(404, e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+}
