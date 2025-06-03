2fd1d305446500c3029107669528d6818f395f5d
Kye <NovusAa12@proton.me>
2025-06-03 20:18:03 +0100

    feat: add ErrorResponse template object

---
+++ backend/src/main/java/com/example/jv_exhibition_api/exception/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.example.jv_exhibition_api.exception;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ErrorResponse {
+    private Integer status;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(Integer status, String message) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = LocalDateTime.now();
+    }
+}
