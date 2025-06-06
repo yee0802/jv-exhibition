de4b2f027fa2cecf7f89afdd19217ba7465b5985
Kye <NovusAa12@proton.me>
2025-06-06 22:03:12 +0100

    feat: add ExhibitionFragment

---
+++ frontend/app/src/main/java/com/example/jv_exhibition/ui/exhibitions/ExhibitionFragment.java
@@ -0,0 +1,67 @@
+package com.example.jv_exhibition.ui.exhibitions;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.jv_exhibition.R;
+import com.example.jv_exhibition.model.Exhibition;
+import com.example.jv_exhibition.ui.mainactivity.RecyclerViewInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExhibitionFragment extends Fragment implements RecyclerViewInterface {
+    private ExhibitionViewModel viewModel;
+    private RecyclerView recyclerView;
+    private List<Exhibition> exhibitionsFromViewModel = new ArrayList<>();
+    private ExhibitionAdapter adapter;
+    private Button createButton;
+    private EditText nameInput;
+
+    public ExhibitionFragment() {
+        super(R.layout.fragment_exhibition);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        viewModel = new ViewModelProvider(this).get(ExhibitionViewModel.class);
+        recyclerView = view.findViewById(R.id.exhibition_recycler_view);
+        nameInput = view.findViewById(R.id.exhibition_name_input);
+        createButton = view.findViewById(R.id.create_exhibition_button);
+
+        adapter = new ExhibitionAdapter(new ArrayList<>(), this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+        viewModel.getExhibitions().observe(getViewLifecycleOwner(), exhibitions -> {
+            exhibitionsFromViewModel = exhibitions;
+            adapter.setExhibitions(exhibitions);
+        });
+
+        createButton.setOnClickListener(v -> {
+            String name = nameInput.getText().toString().trim();
+
+            if (!name.isEmpty()) {
+                viewModel.createExhibition(name);
+                nameInput.setText("");
+            } else {
+                Toast.makeText(getContext(), "Please input an Exhibition name!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewModel.getExhibitions();
+    }
+
+    @Override
+    public void onItemClick(int position) {}
+}
+
